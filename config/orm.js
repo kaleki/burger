cf7e5c13767a2629da2553e86684a770daba9677
kale <intharaksak@gmail.com>
2019-06-18 14:34:04 -0400

    updated orm

--- config/orm.js
+++ config/orm.js
@@ -9,6 +9,29 @@ connection.connect(function(err) {
     console.log('connected as id ' + connection.threadId);
   });
 
-  
+  var orm = {
+      selectAll: function(callBack){
+          connection.query("SELECT * FROM bugers", function(err, result){
+              if (err) throw err;
+              callBack(result)
+          });
+      },
+      insertOne: function(burger_name, callBack){
+            connection.query("INSERT INTO burgers SET ?", {
+                burger_name: burger_name,
+                devoured: false
+            }, function(err, result){
+                if (err) throw err;
+                callBack(result)
+            });
+      },
+      updateOne: function(burger_id, callBack){
+          connection.query("UPDATE burgers SET ? WHERE ?", [{devoured: true}], [{id: burger_id}], function(err, result){
+              if (err) throw err;
+              callBack(result)
+          });
+      }
+  };
+
 
 module.exports = orm;
