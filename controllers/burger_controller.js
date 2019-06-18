2f47966158566c2574617b88fed56f32989e18ae
kale <intharaksak@gmail.com>
2019-06-18 17:40:20 -0400

    updated burger controller js

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -2,4 +2,28 @@ var express = require('express');
 var router = express.Router();
 var burger = require('../models/burger.js');
 
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burger: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+  router.post("/api/burger", function(req,res){
+    burger.insertOne(["burger_name"], [req.body.burger_name], function(result){
+        res.json({id: result.insertId});
+    });
+  });
+
+  router.put("/api/burger/:id", function(req,res){
+      var devoured = "id = " + req.params.id;
+      console.log("devoured", devoured);
+      burger.updateOne({devoured: true}, condition, function(result){
+        res.status(200).end();
+      })
+  })
+
 module.exports = router;
