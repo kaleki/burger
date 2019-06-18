b61a4769a32c19385347a0623b2d69a50340c5e2
kale <intharaksak@gmail.com>
2019-06-18 17:05:37 -0400

    updated burger js

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,22 @@
 var orm = require('../config/orm.js');
 
+var burger = {
+    selectAll: function(callBack){
+        orm.selectAll(function(res){
+            callBack(res);
+        })
+    },
+    insertOne: function(burger_name,callBack){
+        orm.insertOne(burger_name,function(res){
+            callBack(res)
+        })
+    },
+    updateOne: function(burger_id,callBack){
+        orm.updateOne(burger_id, function(res){
+            callBack(res)
+        })
+    }
+
+}
+
 module.exports = burger;
